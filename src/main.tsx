fa6ba63dad304469e885d9b36938765d18f38919
Roque Alarcon <roquealarcon363@gmail.com>
2025-10-29 04:51:08 -0500

    chore: move routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import Providers from '@root/_providers';
-import { router } from '@root/router';
+import { router } from '@root/routes/AppRouter.tsx';
 import '@root/index.css';
 
 createRoot(document.getElementById('root')!).render(
