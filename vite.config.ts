e3fa87310d774d8587a566dfd11446de653767c9
Roque Alarcon <roquealarcon363@gmail.com>
2025-11-07 12:53:49 -0500

    chore: host project in github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/novel-shoppe',
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
